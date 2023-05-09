2f509d2d2fda2e85fc8c4b1825855f4a0dc17409
Maday <76758776+heartlog@users.noreply.github.com>
2023-05-10 04:45:24 +0530

    Create YTredirect-userscript.js
    Rediret m.youtube.com to alternative frontend.

---
+++ redirecting/YTredirect-userscript.js
@@ -0,0 +1,33 @@
+// ==UserScript==
+
+// @name         YT to invidious ♥️
+
+// @namespace    https://viayoo.com/
+
+// @version      0.1
+
+// @description  try to take over the world!
+
+// @author       https://github.com/heartlog/
+
+// @run-at       document-start
+
+// @match        https://*/*
+
+// @grant        none
+
+// ==/UserScript==
+
+(function() {
+
+    'use strict';
+
+    if (location.hostname === "m.youtube.com") {
+
+        const RL = "https://invidious.kavin.rocks" + location.pathname;
+
+        location.href = RL;
+
+        }
+
+})(
