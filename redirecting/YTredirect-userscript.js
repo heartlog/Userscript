2b7af68f22095e786e67f2bd539ef187f6a04250
Maday <76758776+heartlog@users.noreply.github.com>
2023-05-10 05:50:03 +0530

    Update YTredirect-userscript.js
    Added pattern to detect YouTube url

--- redirecting/YTredirect-userscript.js
+++ redirecting/YTredirect-userscript.js
@@ -1,21 +1,32 @@
 // ==UserScript==
 // @name         YT to invidious ♥️
 // @namespace    https://heartlog.github.io/
-// @version      0.2
+// @version      0.3
 // @description  try to take over the world!
 // @author       https://github.com/heartlog/
 // @run-at       document-start
 // @match        https://*/*
+// @match        *://*youtube.com/*
 // @grant        none
 // ==/UserScript==
 
 (function() {
     'use strict';
-
-    if (location.hostname === "m.youtube.com") {
-        const Searchq =  location.pathname + location.search;
+    if (validateYouTubeUrl(location.href) === "T") {
+        const Searchq = location.pathname + location.search;
         var RL = "https://invidious.kavin.rocks" + Searchq;
         location.href = RL;
         }
 
 })();
+
+
+function validateYouTubeUrl(urlT){
+                if (urlT) {
+                    var regExp = /^(?:https?:\/\/)?(?:m\.|www\.)?(?:youtu\.be\/|youtube\.com\/(?:embed\/|v\/|watch\?v=|watch\?.+&v=))((\w|-){11})(?:\S+)?$/;
+                    if (urlT.match(regExp)) {
+                        return "T";
+                    }
+                }
+                return "F";
+}
