0d45dcc5700661696492f8813b91e3fd88334cb0
Maday <76758776+heartlog@users.noreply.github.com>
2023-05-11 05:49:57 +0530

    Create Manganelo.js
    Uses pikwy api to screenshot manga chapter page and give response.

---
+++ ScreenshotASite/Manganelo.js
@@ -0,0 +1,41 @@
+// ==UserScript==
+// @name         manga to Pikwy screenshot ♥️
+// @namespace    https://heartlog.github.com
+// @version      0.3
+// @description  try to take over the world!
+// @author       You
+// @run-at       document-start
+// @match        https://chapmanganato.com/*/*
+// @match        https://chapmanganelo.com/*/*
+// @match        https://manganato.com/*/*
+// @exclude     https://manganato.com/search/*
+// @grant window.close
+// @grant window.onurlchange
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    // URL
+    var PUrl = location.href;
+    PixRedi(PUrl);
+})
+();
+
+
+function PixRedi(FinUrl) {
+  var PixReady = "https://api.pikwy.com/?tkn=125&d=3000&u=" + FinUrl + "&fs=1&w=400&h=812&s=100&z=100&f=jpg&rt=raw";
+  location.href = PixReady;
+}
+/*
+Reference for API parameters
+- tkn : default token is 125
+- d : delay (range 0-6000)
+- u : URL adress of webpage
+- fs : Full screen 1 is on & 0 is off
+- w : width (if increase quality increase)
+- h : height (if increase quality increase)
+- s : scale (if decreased quality decrease)
+- z : zoom (if decreased contant will appear small)
+- f : format png, jpg, html
+- rt : responsetype (raw, json, html, jweb)
+*/
