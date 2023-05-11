3fd37f2454eda0153bdac9eb89c088fa8169301a
Maday <76758776+heartlog@users.noreply.github.com>
2023-05-11 10:59:47 +0530

    update Addon scripts/BlockImg.js

--- Addon scripts/BlockImg.js
+++ Addon scripts/BlockImg.js
@@ -16,15 +16,11 @@
 
 (function() {
     'use strict';
-
     // Block all images on client side
     var images = document.getElementsByTagName('img');
-
     for (var i = 0; i < images.length; i++) {
-
     images[i].src = 'none';
     images[i].style.display = 'none';
     }
-
 
 })()
